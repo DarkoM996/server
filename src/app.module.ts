21a74a6fd8ff42f8c25f158a0dce58b7a2c1bccc
DarkoM996 <darko.mihajlovski96@gmail.com>
2023-06-07 22:41:47 +0200

    DB+Swagger+Resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { MovieModule } from './movie/movie.module';
+import { GenreModule } from './genre/genre.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.env',
+    isGlobal: true
+  }), UserModule, MovieModule, GenreModule,
+  TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: 3306,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [],
+    synchronize: true,
+  }),
+],
   controllers: [AppController],
   providers: [AppService],
 })
